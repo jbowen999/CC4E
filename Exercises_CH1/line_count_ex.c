7f876dc903285bc9a7f9ee2863515b64ac31fae7
jbowen999 <julian.e.bowen@outlook.com>
2024-06-20 15:58:08 -0700

    #

--- Exercises_CH1/line_count_ex.c
+++ Exercises_CH1/line_count_ex.c
@@ -1,17 +1,37 @@
 #include <stdio.h>
 
+// main() /* count lines in input */
+// {
+//     int c, nl;
+
+//     nl = 0;
+//     while ((c = getchar()) != EOF)
+//         if (c == '\n')
+//             ++nl;
+//     printf("%d\n", nl);
+// }
+
+
+// Exercise 1-6. Write a program to count blanks, tabs, and newlines.
 main() /* count lines in input */
 {
-    int c, nl;
+    int c, nl, tc, bc;
 
     nl = 0;
+    tc = 0;
+    bc = 0;
     while ((c = getchar()) != EOF)
         if (c == '\n')
             ++nl;
-    printf("%d\n", nl);
+        else if (c == '\t')
+            ++tc;
+        else if (c == ' ')
+            ++bc;
+    printf("Newlines:%d\n", nl);
+    printf("Tabs:%d\n", tc);
+    printf("Blanks:%d\n", bc);
 }
 
-// Exercise 1-6. Write a program to count blanks, tabs, and newlines.
 
 // Exercise 1-7. Write a program to copy its input to its output, replacing each string of one or more blanks by a single blank.
 
---
+++ ch_1_python/in2out.py
@@ -0,0 +1,3 @@
+a_string = input()
+a_list = a_string.split()
+print(" ".join(a_list))
